2c19bfcee320d79f0aae7c494670fc29af3a1446
kjof <andreeastroem@gmail.com>
2025-09-30 08:01:58 +0200

    Revert "feat: refactor to a DDD approach"
    This reverts commit 216cc7f35f2c2d22071df8ff2a2328236e3d442f.

---
+++ src/Actor/actor.cpp
@@ -0,0 +1,15 @@
+#include "actor.h"
+
+Actor::Actor() : health(100), damage(10) {}
+
+Actor::~Actor() {}
+
+void Actor::update()
+{
+    // Update actor state
+}
+
+void Actor::render()
+{
+    // Render actor
+}
